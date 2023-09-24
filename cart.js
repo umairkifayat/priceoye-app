eb82a6fc3b6abb2b0121d81d2c9243df522a1acf
umair kifayat <umairkifayat3@gmail.com>
2023-09-24 06:24:49 -0700

    mobile-mart

--- cart.js
+++ cart.js
@@ -4,19 +4,36 @@ function gohome(params) {
 }
 
 
-const additem = document.getElementById("#main-div")
-const data = localStorage.getItem("addcartarr")
-const addcartarr = JSON.parse(data)
+const maindiv = document.getElementById("main-div")
+const div = document.getElementById("div")
+
+
+
+let data = localStorage.getItem("addcartarr")
+let retarray = JSON.parse(data)
 // console.log(addcartarr);
 
 
 
 function render() {
-   for (let i = 0; i < addcartarr.length; i++) {
-//  console.log(addcartarr[i]);    
+   for (let i = 0; i < retarray.length; i++) {
+retarray[i].totalprice =+ `${retarray[i].price} * ${retarray[i].quantity} `
+// console.log(addcartarr[i]);
+maindiv.innerHTML += `<div class ="c-div main ">
+<h2 > ${retarray[i].brand}</h2>
+<img class="js-img" src=" ${retarray[i].img}" alt="">
+<p  class = "h4"> <b>Ram: ${retarray[i].ram}</p></b>
+<p  class = "h4"><b> Rom: ${retarray[i].rom}</p></b>
+<p  class = "h4"><b>Camera: ${retarray[i].camera}</p></b>
+<p  class = "h4"><b>Price: ${retarray[i]} </p></b>
+<p  class = "h4"><b>Price: ${retarray[i].quantity} </p></b>
+<p  class = "h4"><b>Price: ${retarray[i].TotalPrice} </p></b>
+<button onclick = "addtocart(${i})" class="btn">ADD to cart </button>
+</div>
+`
    }
 }
-
+render()
 // const div = document.querySelector("#main-div");
 
 
